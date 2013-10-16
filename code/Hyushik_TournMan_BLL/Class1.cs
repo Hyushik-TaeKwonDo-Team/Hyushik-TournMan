5c67f2a5d581ec35340661a09a0c15440d1e7ffb
McAfee <pjm4439@rit.edu>
2013-10-16 13:35:21 -0400

    Rejiggered project structure

---
+++ code/Hyushik_TournMan_BLL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyushik_TournMan_BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ code/Hyushik_TournMan_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hyushik_TournMan.MvcApplication" Language="C#" %>
