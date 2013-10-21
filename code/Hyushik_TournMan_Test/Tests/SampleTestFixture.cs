6d0f704084d058cbf3c6cf82b48d58c84f556415
McAfee <pjm4439@rit.edu>
2013-10-21 17:35:36 -0400

    Added some sample tests

---
+++ code/Hyushik_TournMan_Test/Tests/SampleTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Hyushik_TournMan_Test.Tests
+{
+    [TestFixture]
+    public class SampleTestFixture
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+
+    }
+}
