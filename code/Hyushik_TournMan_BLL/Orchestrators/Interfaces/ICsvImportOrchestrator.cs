644b90ea79b712a7995eb9a927a16574aea10713
McAfee <pjm4439@rit.edu>
2013-10-22 12:33:22 -0400

    added orchestrator interface for CSV

---
+++ code/Hyushik_TournMan_BLL/Orchestrators/Interfaces/ICsvImportOrchestrator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyushik_TournMan_BLL.Orchestrators.Interfaces
+{
+    public interface ICsvImportOrchestrator
+    {
+        public void importParticipantCsvFile();
+    }
+}
