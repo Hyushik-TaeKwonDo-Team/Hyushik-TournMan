1f0d23716a857be74de1572ba63461b401e13ba3
Craig Felton <feltocraig@gmail.com>
2013-11-03 15:13:43 -0500

    added css files

--- code/Hyushik_TournMan_Web/App_Start/BundleConfig.cs
+++ code/Hyushik_TournMan_Web/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace Hyushik_TournMan_Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include("~/Content/bootstrap.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/main.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
