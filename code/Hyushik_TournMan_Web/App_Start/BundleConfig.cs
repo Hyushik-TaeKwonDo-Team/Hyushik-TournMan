5508f2589b816a7f69f4e11c4c16821770e2c19a
Craig Felton <feltocraig@gmail.com>
2013-11-03 16:13:52 -0500

    modified css and main page look

--- code/Hyushik_TournMan_Web/App_Start/BundleConfig.cs
+++ code/Hyushik_TournMan_Web/App_Start/BundleConfig.cs
@@ -23,8 +23,11 @@ namespace Hyushik_TournMan_Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include("~/Content/bootstrap.css"));
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/main.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/main.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/main.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
