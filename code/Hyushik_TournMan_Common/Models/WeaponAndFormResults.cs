d678328e88f26f304900bacd713f265eb4655c5c
McAfee <pjm4439@rit.edu>
2014-02-07 17:56:21 -0500

    Model for Weapons/Forms

---
+++ code/Hyushik_TournMan_Common/Models/WeaponAndFormResults.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyushik_TournMan_Common.Models
+{
+    public abstract class WeaponOrFormResult{
+
+        public virtual Participant Participant { get; set; }
+        public virtual List<WeaponAndFormJudgeScore> JudgeScores {get; set;}
+
+        protected void Initialize()
+        {
+            JudgeScores = new List<WeaponAndFormJudgeScore>();
+        }
+    }
+
+    public class WeaponAndFormJudgeScore{
+        [Key]
+        public long Id { get; set; }
+        //has to be hand set, because db split
+        public long Judge_UserId { get; set; }
+        public int Score { get; set; }
+    }
+
+    public class WeaponResult : WeaponOrFormResult
+    {
+        [Key]
+        public long Id { get; set; }
+        public WeaponResult()
+        {
+            Initialize();
+        }
+    }
+
+    public class FormResult : WeaponOrFormResult
+    {
+        [Key]
+        public long Id { get; set; }
+        public FormResult()
+        {
+            Initialize();
+        }
+    }
+}
--- code/Hyushik_TournMan_DAL/Contexts/TournManContext.cs
+++ code/Hyushik_TournMan_DAL/Contexts/TournManContext.cs
@@ -17,12 +17,18 @@ namespace Hyushik_TournMan_DAL.Contexts
 
         public DbSet<Participant> Participants { get; set; }
         public DbSet<BoardSizeCount> BoardSizeCounts { get; set; }
+
         public DbSet<Tournament> Tournaments { get; set; }
+
         public DbSet<Technique> Techniques { get; set; }
         public DbSet<BreakingResult> BreakingResults { get; set; }
         public DbSet<Station> Stations { get; set; }
         public DbSet<BreakingJudgeScore> BreakingJudgeScores { get; set; }
         public DbSet<TechniqueValue> TechniqueValues { get; set; }
+
+        public DbSet<WeaponResult> WeaponResults { get; set; }
+        public DbSet<FormResult> FormResults { get; set; }
+        public DbSet<WeaponAndFormJudgeScore> WeaponAndFormJudgeScores { get; set; }
         
         
 
