df2e98f732753967547e54e832843d1f2c2a66fd
McAfee <pjm4439@rit.edu>
2014-04-13 12:59:26 -0400

    Making headway on double as int fails

---
+++ code/Hyushik_TournMan_Web/Scripts/hyushik.js
@@ -0,0 +1,16 @@
+﻿function hasHtml5Validation() {
+    return typeof document.createElement('input').checkValidity === 'function';
+}
+$(function () {
+    if (hasHtml5Validation()) {
+        console.log("going!");
+        $('form').submit(function (e) {
+            if (!this.checkValidity()) {
+                e.preventDefault();
+                $(this).addClass('invalid');
+            } else {
+                $(this).removeClass('invalid');
+            }
+        });
+    }
+});
